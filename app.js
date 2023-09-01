7ff57e800c4847fb749e789ca7ce99ba4219a671
Nahid Parvez <nahid.parvez@bjitacademy.com>
2023-09-01 14:33:13 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const ProductRouter = require("./routes/Product");
+const UserRouter = require("./routes/User");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const databaseConnection = require("./config/database");
+
+dotenv.config();
+
+app.use(cors({ origin: "*" }));
+app.use(express.json()); // Parses data as JSON
+app.use(express.text()); // Parses data as text
+app.use(express.urlencoded({ extended: true })); // Parses data as urlencoded
+
+app.use("/products", ProductRouter);
+app.use("/users", UserRouter);
+app.use("/orders", OrderRouter);
+
+databaseConnection(() => {
+    app.listen(8000, () => {
+        // console.log(process.env.TEST_DB);
+        console.log("Server is running on port 8000");
+    });
+})
